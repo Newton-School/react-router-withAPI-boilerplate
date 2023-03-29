a8d040c1950b3292f5aa2a5921034c4cd6be4a9d
RaghavSabby <ragsabharwal369@gmail.com>
2023-03-29 18:15:40 +0530

    Done

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/App.css';
+import Navbar from './Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Top from './Top';
+import Contact from './Contact';
+
+const App = () => {
+
+  return (
+    <div id="main">
+      <Navbar />
+      <div className='container'>
+        <Route path="/"  />
+        <Route path="/top-10" />
+        <Route path="/contact"  />
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
